1c68178b9cc9ecc0919d1cea6165b1b94fb7876c
Anto <bueno@uoc.edu>
2025-02-08 19:35:34 +0100

    chore: actualizar información de contacto y redes sociales en el perfil

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,27 +1,27 @@
-import IconDribbble from "../components/icons/dribbble.astro";
 import IconGithub from "../components/icons/github.astro";
 import IconLinkedin from "../components/icons/linkedin.astro";
 import IconTwitter from "../components/icons/twitter.astro";
+import IconWebsite from "../components/icons/website.astro";
 
 export const SOCIALS = [
   {
     name: "Github",
-    url: "https://github.com/thevrus",
+    url: "https://github.com/antoniobuen0",
     icon: IconGithub,
   },
   {
     name: "Linkedin",
-    url: "https://www.linkedin.com/in/vrusin/",
+    url: "https://www.linkedin.com/in/buen0/",
     icon: IconLinkedin,
   },
   {
     name: "Twitter",
-    url: "https://twitter.com/rusin_vadim",
+    url: "https://x.com/polimathia",
     icon: IconTwitter,
   },
   {
-    name: "Dribbble",
-    url: "https://dribbble.com/iamvr",
-    icon: IconDribbble,
+    name: "Website",
+    url: "https://nous.cat/",
+    icon: IconWebsite,
   },
 ];
