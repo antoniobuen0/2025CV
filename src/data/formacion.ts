f5e39bc5088c2b7d3653483b10f3eec9c02820dc
Anto <bueno@uoc.edu>
2025-02-08 23:03:38 +0100

    chore: agregar nueva sección de formación con detalles de educación y certificaciones

--- src/data/formacion.ts
+++ src/data/formacion.ts
@@ -1,1 +1,49 @@
-export const FORMACION = 'Texto genérico de formación.'
+import type { Props as EducationProp } from "../components/education/props";
+
+export const EDUCATION: EducationProp[] = [
+  {
+    dates: "2024",
+    degree: "Data Science Mastery",
+    institution: "DS4B",
+    institutionUrl: "https://www.ds4b.com", // Actualiza la URL si es necesario
+    description:
+      "Fundamentos de Data Science: Python, estadística, numpy, pandas. Business Analytics: técnicas analíticas avanzadas para el negocio, generación de insights, construcción de business case. Machine Learning: algoritmos, entrenamiento de modelos, creación de data apps, sistemas predictivos de alto rendimiento, forecasting.",
+    logo: "/logos/ds4b.jpeg",
+  },
+  {
+    dates: "2024",
+    degree: "IBM Data Science Professional Certificate",
+    institution: "IBM",
+    institutionUrl: "https://www.ibm.com", // Actualiza la URL si es necesario
+    description:
+      "Bases de datos, visualización de datos, análisis estadístico, modelos predictivos, algoritmos de machine learning, Python, SQL.",
+    logo: "/logos/ibm.jpeg",
+  },
+  {
+    dates: "2024",
+    degree: "Datahack - PowerBI / SQL Server",
+    institution: "Datahack",
+    institutionUrl: "https://www.datahack.com", // Actualiza la URL si es necesario
+    description:
+      "Bases de datos, visualización de datos, análisis estadístico, modelos predictivos, algoritmos de machine learning, Python, SQL.",
+    logo: "/logos/datahack.jpeg",
+  },
+  {
+    dates: "2019 — 2025",
+    degree: "BSc Psicología",
+    institution: "Universitat Oberta de Catalunya",
+    institutionUrl: "https://www.uoc.edu", // Actualiza la URL si es necesario
+    description:
+      "Investigaciones sobre superdotación intelectual.",
+    logo: "/logos/uoc.jpeg",
+  },
+  {
+    dates: "2012 — 2014",
+    degree: "BSc Ingeniería informática (Inc.)",
+    institution: "Universitat d'Alacant",
+    institutionUrl: "https://www.ua.es", // Actualiza la URL si es necesario
+    description:
+      "Grupo Alto Rendimiento Académico.",
+    logo: "/logos/ua.jpeg",
+  },
+];
